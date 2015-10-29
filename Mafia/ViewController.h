86b4f5976e24ca2ce7bcdfbf194cf36cd560586c
keakalamudo <kelo.akalamudo@gmail.com>
2015-10-28 17:08:58 -0700

    Empty Project

---
+++ Mafia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mafia
+//
+//  Created by Kelo Akalamudo on 10/28/15.
+//  Copyright © 2015 Kelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mafia/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Mafia
+//
+//  Created by Kelo Akalamudo on 10/28/15.
+//  Copyright © 2015 Kelo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
